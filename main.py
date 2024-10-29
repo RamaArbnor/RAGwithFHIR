688a56e6303c62a22e05e04ebb7d44d22197474c
Arbnor Rama <arama@york.citycollege.eu>
2024-10-29 13:52:00 +0100

    dotenv fix

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ import os
 import faiss
 import numpy as np
 from sentence_transformers import SentenceTransformer
-from llm import generate_answer_with_llm
+import requests
+from dotenv import load_dotenv
+# from llm import generate_answer_with_llm
 
 # Path to your FHIR JSON files
 fhir_data_folder = "Dataset"
+load_dotenv()
+url = "https://api.arliai.com/v1/chat/completions"
+ARLIAI_API_KEY = os.environ.get('API_KEY')
+
 
 # Load all the JSON files in the directory
 def load_fhir_data(folder_path):
@@ -96,6 +102,43 @@ def search(query, k=5):
     
     return results
 
+def generate_answer_with_llm(query, retrieved_entries):
+    payload = json.dumps({
+    "model": "Meta-Llama-3.1-8B-Instruct",
+    "messages": [
+        {"role": "system", "content": f"""
+        You are an assistant with medical knowledge. Use the following FHIR data entries to provide a summarized answer.
+
+        User Query: "{query}"
+
+        Retrieved Data:
+        {retrieved_entries}
+
+        Provide a structured answer relevant to the query.
+        """},
+
+    ],
+    "repetition_penalty": 1.1,
+    "temperature": 0.7,
+    "top_p": 0.9,
+    "top_k": 40,
+    "max_tokens": 1024,
+    "stream": False
+    })
+    headers = {
+    'Content-Type': 'application/json',
+    'Authorization': f"Bearer {ARLIAI_API_KEY}"
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+    # Extract and return only the text from the response
+    if response.status_code == 200:
+        try:
+            return response.json().get('choices', [{}])[0].get('message', {}).get('content', '')  # if the response is JSON
+        except json.JSONDecodeError:
+            return response.text  # if it's plain text
+    else:
+        return f"Error: {response.status_code} - {response.text}"
 
 # Example: Query the index
 query_text = "Retrieve information on general health metrics like height, weight, BMI, blood pressure, heart rate, and any recent viral infections for  Bart73 "
